35170bfd439b60fecab4aca4d8cd488defc19ded
Aditya Raj <64183241+adi5677@users.noreply.github.com>
2025-02-07 15:54:42 +0530

    Comparision Operators in Js

---
+++ 01_Basics/04_Comparision.js
@@ -0,0 +1,33 @@
+// console.log(2 < 3);
+// Output: true
+// console.log(2 <= 3);
+// Output: true
+// console.log(2 > 5);
+// Output: false
+// console.log(8 >= 12);
+// console.log(8 != 15);
+
+
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+
+console.log(undefined > 0);
+console.log(undefined == 0);
+console.log(undefined >= 0);
+
+//=== strict check 
+
+console.log("2" === 2);
+console.log(2 == "2");
+
+
+
+
+
+
+
+
+
