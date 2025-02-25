5f4bd1d04e2a869d296ba8a803cf25854b449a27
Aditya Raj <64183241+adi5677@users.noreply.github.com>
2025-02-25 21:19:21 +0530

    While Loop

---
+++ 05_Loops/two.js
@@ -0,0 +1,31 @@
+//While Loop
+// while (condition) {
+    
+// while example
+let index = 0;
+while (index <= 25) {
+    // console.log(`value of index is ${index}`);
+    // index++;
+    index = index + 2
+}
+
+
+let myArray = ['flash', "batman", "superman"]
+
+let arr = 0
+while (arr < myArray.length) {
+    // console.log(`value is ${myArray[arr]}`);
+    arr = arr + 1
+}
+
+
+//Do While Loop
+
+
+let score = 1
+// let score = 11 // this is a exception case
+do {
+    console.log(`score is ${score}`);
+    score = score + 1
+    
+} while (score <= 10);
